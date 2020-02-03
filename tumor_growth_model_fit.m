d47ad52c0e7e5d29645449bdeaf9516c48fafcfd
Cristian Axenie <cristian.axenie@gmail.com>
2020-02-03 19:58:14 +0100

    Refactor code. Cleanup.

--- tumor_growth_model_fit.m
+++ tumor_growth_model_fit.m
@@ -9,54 +9,64 @@ function tumor_growth_model_fit()
 
 % This program attempts to find values for r and K in logistic
 % model that best fit a given set of data by using fminsearch
-clear all;
+clear;
 close all;
-global T M r K
+global T M r K model
+% choose model
+% 'logistic', 'vonBertalanffy', 'Gompertz'
+model =  'Gompertz'; 
 x0=[0.8; 5];
-[min, fval]=fminsearch(@er,x0,optimset('TolX',1e-6,'MaxIter',200));
-min(1)=r; 
-min(2)=K;
-[t,y]=ode23s(@vonbertalanffy,[1 5],0.5);
+[min, ~]=fminsearch(@er,x0,optimset('TolX',1e-6,'MaxIter',200));
+r = min(1); 
+K = min(2);
+% choose model
+% @logistic, @vonbertalanffy, @gompertz
+[t,y]=ode23s(@gompertz,[1 5],0.5); 
 plot(t,y,T,M,'r*');
+set(gcf, 'color', 'w');
+title(sprintf('%s growth model', model));
+legend('fit model', 'data points (mass/day)');
+box off;
 
 end
 
 %function for ode solver
 function z=er(x)
-global T M r K
+global T M r K model
 tt=0:1:60;
 % data points from experiment
 T=[1,2,3,4,5]'; 
 M=[0.5,1,3,4,4.5]';
 r=x(1); 
 K=x(2);
-y0 = [0.1]; 
-y = y0;
-model =  'vonBertalanffy'; % 'logistic', 'vonBertalanffy', 'Gompertz'
+y0 = 0.1; 
 switch model
     case 'logistic'
-        [t1, y1] = ode23s(@logistic, tt, y0);
+        [~, y1] = ode23s(@logistic, tt, y0);
     case 'vonBertalanffy'
-        [t1, y1] = ode23s(@vonbertalanffy, tt, y0);
+        [~, y1] = ode23s(@vonbertalanffy, tt, y0);
     case 'Gompertz'
-        [t1, y1] = ode23s(@gompertz, tt, y0);
+        [~, y1] = ode23s(@gompertz, tt, y0);
 end
 z=sum((y1(T)-M).^2); % minimize the squared error criteria
 end
 
-function yp = logistic(t,y)
+% logistic model
+function yp = logistic(~,y)
 global r K
 yp = y; 
 yp(1) = r*y(1)*(1 - y(1)/K);
 end
 
-function yp = vonbertalanffy(t,y)
+% von Bertalanffy model
+function yp = vonbertalanffy(~,y)
 global r K
 yp = y; 
 yp(1) = r*y(1)*(1/(y(1)^(1/3)) - 1/K);
 end
 
-function yp = gompertz(t,y)
+% Gompertz model
+function yp = gompertz(~,y)
 global r K
 yp = y; 
 yp(1) = r*y(1)*(1/K - log(y(1)));
