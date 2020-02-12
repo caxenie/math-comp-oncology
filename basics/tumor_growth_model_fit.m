c91364abeee8f704c6bd6233d9482d36eb6bf1ca
Cristian Axenie <cristian.axenie@huawei.com>
2020-02-12 11:27:41 +0100

    Add Bernoulli model and other models overview.

--- basics/tumor_growth_model_fit.m
+++ basics/tumor_growth_model_fit.m
@@ -11,17 +11,18 @@ function tumor_growth_model_fit()
 % model that best fit a given set of data by using fminsearch
 clear;
 close all;
-global T M r K model
+global T M r K model miu
 % choose model
-% 'logistic', 'vonBertalanffy', 'Gompertz'
-model =  'Gompertz'; 
+%% 'logistic', 'vonBertalanffy', 'Gompertz', 'Bernoulli'
+model =  'Bernoulli'; 
 x0=[0.8; 5];
 [min, ~]=fminsearch(@er,x0,optimset('TolX',1e-6,'MaxIter',200));
 r = min(1); 
 K = min(2);
+miu = 2.7;
 % choose model
-% @logistic, @vonbertalanffy, @gompertz
-[t,y]=ode23s(@gompertz,[1 5],0.5); 
+%% @logistic, @vonbertalanffy, @gompertz, @bernoulli
+[t,y]=ode23s(@bernoulli,[1 5],0.5); 
 plot(t,y,T,M,'r*');
 set(gcf, 'color', 'w');
 title(sprintf('%s growth model', model));
@@ -32,13 +33,14 @@ end
 
 %function for ode solver
 function z=er(x)
-global T M r K model
+global T M r K model miu
 tt=0:1:60;
 % data points from experiment
 T=[1,2,3,4,5]'; 
 M=[0.5,1,3,4,4.5]';
 r=x(1); 
 K=x(2);
+miu = 2.7;
 y0 = 0.1; 
 switch model
     case 'logistic'
@@ -47,6 +49,8 @@ switch model
         [~, y1] = ode23s(@vonbertalanffy, tt, y0);
     case 'Gompertz'
         [~, y1] = ode23s(@gompertz, tt, y0);
+    case 'Bernoulli'
+        [~, y1] = ode23s(@bernoulli, tt, y0);
 end
 z=sum((y1(T)-M).^2); % minimize the squared error criteria
 end
@@ -70,4 +74,11 @@ function yp = gompertz(~,y)
 global r K
 yp = y; 
 yp(1) = r*y(1)*(1/K - log(y(1)));
+end
+
+% Bernoulli model
+function yp = bernoulli(~,y)
+global r K miu
+yp = y; 
+yp(1) = r*y(1)*(1 - (y(1)^(miu - 1))/K);
 end
